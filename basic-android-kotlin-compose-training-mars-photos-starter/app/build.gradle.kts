a2292d20c7fb744b5ba66568ccc36d1d70a4daa4
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-03 14:25:56 -0600

    Avance: FINALIZADO. Se desserializo la respuesta JSON. Se fanilizo la practica Tema: Cómo obtener datos de Internet Laboratorio: Cómo obtener datos de Internet url: https://developer.android.com/codelabs/basic-android-kotlin-compose-getting-data-internet?hl=es-419&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-5-pathway-1%3Fhl%3Des-419%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-getting-data-internet#0

--- basic-android-kotlin-compose-training-mars-photos-starter/app/build.gradle.kts
+++ basic-android-kotlin-compose-training-mars-photos-starter/app/build.gradle.kts
@@ -69,6 +69,12 @@ android {
 
 dependencies {
 
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Retrofit with Scalar Converter
+    implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+
     // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     implementation("androidx.activity:activity-compose:1.7.2")
