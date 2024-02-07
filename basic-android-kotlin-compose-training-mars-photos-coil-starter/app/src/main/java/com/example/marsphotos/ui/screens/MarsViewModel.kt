ea8a5a6758c7026d35de5394925350e47338566f
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2024-02-07 13:52:15 -0600

    Avance: Se finalizo el laboratorio Tema: Cómo obtener datos de Internet Laboratorio: Cómo cargar y mostrar imágenes de Internet url: https://developer.android.com/codelabs/basic-android-kotlin-compose-load-images?hl=es-419&continue=https%3A%2F%2Fdeveloper.android.com%2Fcourses%2Fpathways%2Fandroid-basics-compose-unit-5-pathway-2%3Fhl%3Des-419%23codelab-https%3A%2F%2Fdeveloper.android.com%2Fcodelabs%2Fbasic-android-kotlin-compose-load-images#1

--- basic-android-kotlin-compose-training-mars-photos-coil-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
+++ basic-android-kotlin-compose-training-mars-photos-coil-starter/app/src/main/java/com/example/marsphotos/ui/screens/MarsViewModel.kt
@@ -35,7 +35,7 @@ import java.io.IOException
  * UI state for the Home screen
  */
 sealed interface MarsUiState {
-    data class Success(val photos: String) : MarsUiState
+    data class Success(val photos: List<MarsPhoto>) : MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
@@ -60,9 +60,10 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val listResult = marsPhotosRepository.getMarsPhotos()
+                val result = marsPhotosRepository.getMarsPhotos()
                 MarsUiState.Success(
-                    "Success: ${listResult.size} Mars photos retrieved"
+                    //"First Mars image URL: ${result.imgSrc}"
+                    result
                 )
             } catch (e: IOException) {
                 MarsUiState.Error
